21abcdcacd8bb81ee9e08f44f6f0df1a501e3eec
ren5602 <naziz5602@gmail.com>
2025-10-29 14:09:05 +0700

    Week-10 Praktikum 1 Dasar State

---
+++ Week-10/master_plan/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '/views/plan_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: PlanScreen(),
+    );
+  }
+}
